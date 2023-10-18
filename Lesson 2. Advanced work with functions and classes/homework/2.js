7727f11ab3f97e3c89588b578765437f7936e9cf
Lera <valeriiakolesnikova21@gmail.com>
2023-10-18 13:02:02 +0500

    homework 2 finished

--- Lesson 2. Advanced work with functions and classes/homework/2.js
+++ Lesson 2. Advanced work with functions and classes/homework/2.js
@@ -52,4 +52,58 @@ const initialData = [
   },
 ];
 
+const productsSelect = document.querySelector("#productField");
+const reviewField = document.querySelector("#reviewField");
+const reviewForm = document.querySelector("#reviewForm");
+const reviewsList = document.querySelector("#reviews");
+const errorField = document.querySelector("#errorField");
 
+let currentReviewId = initialData.reduce((max, review) => {
+  const maxReviewId = review.reviews.reduce((a, r) => a > r.id ? a : r.id, 0);
+  return Math.max(max, maxReviewId);
+}, 0);
+
+initialData.forEach(item => {
+  const field = `<option value="${item.product}">${item.product}</option>`;
+  productsSelect.insertAdjacentHTML("beforeend", field);
+});
+
+const renderError = (msg) => {
+  errorField.textContent = msg ? msg : "";
+};
+
+const renderReviews = () => {
+  reviewsList.innerHTML = "";
+  initialData.forEach(item => {
+    const field = `<dt>${item.product}</dt>`;
+    const subField = `<dd><ul>${item.reviews.reduce((str, review) => str + "<li>" + review.text + "</li>", "")}</ul></dd>`;
+    const insertStr = field + subField;
+    reviewsList.insertAdjacentHTML("beforeend", insertStr);
+  });
+};
+
+renderReviews();
+
+reviewForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  renderError();
+  try {
+    if (!initialData.some(item => item.product === productsSelect.value)) {
+      renderError("Вы не выбрали товар");
+      return;
+    }
+    const review = reviewField.value;
+    if (review.length < 50 || review.length > 500) {
+      throw new Error("Ваш отзыв должен быть длиной от 50 до 500 символов");
+    }
+    const prod = initialData.find(el => el.product === productsSelect.value);
+    console.log(prod);
+    prod.reviews.push({id: ++currentReviewId, text: review});
+    console.log(initialData);
+  } catch (e) {
+    renderError(e.message);
+  } finally {
+    renderReviews();
+  }
+
+});
